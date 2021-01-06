d4641f35717131ddd7616bffcf48dbcdc0f2792c
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2021-01-06 17:02:50 -0500

    first page

---
+++ index.js
@@ -0,0 +1,11 @@
+// window.onload = function () {
+//     Particles.init({
+//         selector: '.background',
+//         connectParticles: true,
+//         speed: 1,
+//         color: "#FFFFFF",
+//         minDistance: 200,
+//         maxParticles: 200,
+//         sizeVariations: 5
+//     });
+// };
---
+++ js/logo.js
@@ -0,0 +1,5 @@
+// const logo = document.querySelectorAll("#download-btn path");
+//
+// for (let i = 0; i < logo.length; i++) {
+//     console.log(logo[i].getTotalLength());
+// }
